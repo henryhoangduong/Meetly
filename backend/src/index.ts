00d952d13874a5c03ae713112407f909c4cb7eda
henryhoangduong <henryhoangduong@gmail.com>
2025-05-09 20:50:25 +0700

    backend: asyncHandler and errorHandler

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,27 @@
 import 'dotenv/config'
 import { config } from './config/app.config'
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import cors from 'cors'
+import { HTTPSTATUS } from './config/http.config'
+import { errorHandler } from './middleware/errorHandler.middleware'
 
 const app = express()
+const BASE_PATH = config.BASE_PATH
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(errorHandler)
+
+app.use(
+  cors({
+    origin: config.FRONTEND_ORIGIN,
+    credentials: true
+  })
+)
+
+app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  res.status(HTTPSTATUS.OK).send('hello world')
+})
 app.listen(config.PORT, async () => {
   console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`)
 })
